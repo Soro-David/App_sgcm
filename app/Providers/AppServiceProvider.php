39751c20ef1b1bfbefcfa861a57f14d13c03cf79
Soro-David <soroddavid63@gmail.com>
2026-02-17 08:32:51 +0000

    fix: 17-02-2026 restruturation des table de migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,36 +2,24 @@
 
 namespace App\Providers;
 
-use Illuminate\Cache\RateLimiting\Limit;
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
 
-class RouteServiceProvider extends ServiceProvider
+class AppServiceProvider extends ServiceProvider
 {
-    public const HOME = '/home';
-
-    public function boot(): void
+    /**
+     * Register any application services.
+     */
+    public function register(): void
     {
-        $this->configureRateLimiting();
-
-        $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-        });
-        Schema::defaultStringLength(1050);
+        //
     }
 
-    protected function configureRateLimiting(): void
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
     {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
+        Schema::defaultStringLength(1050);
     }
 }
